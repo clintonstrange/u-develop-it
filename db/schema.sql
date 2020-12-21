a232915add70dade5811227d3d23812c841b51f0
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2020-12-21 13:56:43 -0600

    added voter table, routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,21 @@
+DROP TABLE IF EXISTS voters;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
+
+CREATE TABLE voters (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE parties (
     id INTEGER PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT
 );
+
 CREATE TABLE candidates (
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
